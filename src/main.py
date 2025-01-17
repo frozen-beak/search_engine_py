0a26338f440fde0e85cf677570bcb6faf0b4213c
adii <77727680+frozen-beak@users.noreply.github.com>
2025-01-17 21:31:02 +0530

    added api route for health_check

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import JSONResponse
 from .search import SearchEngine
 
 app = FastAPI()
@@ -7,9 +8,17 @@ searchEngine = SearchEngine()
 
 
 @app.get("/api/search")
-def index(query: str):
+def search_query(query: str):
     return searchEngine.perform_search(query)
 
 
-# Mount static files
+@app.get("/api/health")
+def health_check():
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={"status": "ok"},
+    )
+
+
+# Serve static files over default route
 app.mount("/", StaticFiles(directory="./src/public", html=True), name="static")
